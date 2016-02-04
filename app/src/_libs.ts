5b417539a1ea3e7454e0b11ea2190ef38732a4ba
Srinivas Gurajala <Srinivasa.Gurajala@Symphonyteleca.com>
2016-02-04 14:33:28 +0530

    app folder deleted

--- app/src/_libs.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../typings/tsd.d.ts" />
-/// <reference path="./typings/ionic.d.ts" />
-/// <reference path="./typings/Screen.d.ts" />
-/// <reference path="./typings/IsTablet.d.ts" />
-/// <reference path="./typings/InAppBrowser.d.ts" />
